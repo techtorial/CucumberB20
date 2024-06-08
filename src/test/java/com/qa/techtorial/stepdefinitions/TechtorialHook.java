e6d335ab9a5a01a38dd4560a9d128e868d7b0801
techtorial <50930535+techtorial@users.noreply.github.com>
2024-06-08 10:57:02 -0500

    Latest Code

--- src/test/java/com/qa/techtorial/stepdefinitions/TechtorialHook.java
+++ src/test/java/com/qa/techtorial/stepdefinitions/TechtorialHook.java
@@ -10,17 +10,17 @@ import utils.DriverHelper;
 
 public class TechtorialHook {
 
-    public WebDriver driver;
-
-    @Before //Please make sure you import from "IO CUCUMBER"
-    public void setup(){
-        driver= DriverHelper.getDriver();
-        driver.get(ConfigReader.readProperty("qa_url"));
-    }
-
-    @After
-    public void tearDown(Scenario scenario){
-        BrowserUtils.getScreenShotCucumber(driver,scenario);
-        driver.quit();
-    }
+//    public WebDriver driver;
+//
+//    @Before //Please make sure you import from "IO CUCUMBER"
+//    public void setup(){
+//        driver= DriverHelper.getDriver();
+//        driver.get(ConfigReader.readProperty("qa_url"));
+//    }
+//
+//    @After
+//    public void tearDown(Scenario scenario){
+//        BrowserUtils.getScreenShotCucumber(driver,scenario);
+//        driver.quit();
+//    }
 }
